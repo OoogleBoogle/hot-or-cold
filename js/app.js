44830b66e5a425ec034c44e29bf72113bfc75988
dengeist <eliasjmason@gmail.com>
2016-04-25 13:03:32 -0700

    Moves logic out of core game mode and into player method.

--- js/app.js
+++ js/app.js
@@ -18,22 +18,25 @@
     var game, player1, player2;
 
     var Model = function() {
+        this.activePlayer = 0; // Will increment as needed
         this.secretNumber = Math.floor(Math.random() * (100 - 1)) + 1;
         console.log(this.secretNumber);
-        // this.currentPlayer = player;
-        this.currentGuess = "";
-        this.guessList = [];
         this.playerList = [];
-        this.totalGuesses = 0;
-        this.distance = 0;
+        // this.currentPlayer = player;
     };
 
     Model.prototype.makePlayer = function(name) {
         var player = {};
         player.name = name;
         player.score = 0;
-        player.number = this.playerList.length + 1;
+        player.currentGuess = "";
+        player.guessList = [];
+        player.distance = 0;
         this.playerList.push(player);
+    };
+
+    Model.prototype.incrementPlayer = function(){
+            this.activePlayer += 1;
     };
 
     var Game = function(player) {
@@ -44,105 +47,107 @@
         $('#userGuess').val('');
     };
 
-    Game.prototype.validateGuess = function(guess) {
-        var result;
-        this.totalGuesses++;
-        this.currentGuess = guess.toString();
-        // check it's an actual number
-        if (typeof guess === 'number') {
-            // if number is between 1 & 100
-            if (guess >= 1 && guess <= 100) {
-                //result =
-                result = this.giveFeedback(guess);
-                // else say so
-            } else {
-                result = "Your guess needs to be between 1 and 100";
-            }
-        } else {
-            result = "That's not even a number";
-        }
-        return result;
-    };
-    Game.prototype.addGuess = function() {
-
-    };
-
-    Game.prototype.giveFeedback = function(guess) {
-        var result;
-        // if it's correct...
-        if (guess === this.secretNumber) {
-            result = "You Got It!!! The number was " + this.secretNumber;
-            addPlayerScore(this.currentPlayer, this.totalGuesses);
-            startNewRound(this.currentPlayer);
-        }
-        // check this isn't the first guess
-        else if (this.distance > 0) {
-            // and see if they've already guessed this number before
-            $('#guessList li').each(function() {
-                var prevGuess = parseInt($(this).text());
-                if (prevGuess === guess) {
-                    result = "You've already had that number";
-                    return result;
-                }
-            });
-            // if not calculate current guesses distance from the secret number
-            var currentDistance = Math.abs(guess - this.secretNumber);
-            // if they're equidistant from the answer to their previous guess
-            if (currentDistance === this.distance) {
-                result = "OOOO! Equidistant!!!";
-                // else if further away...
-            } else if (currentDistance > this.distance) {
-                result = "Colder :(";
-                // or closer...
-            } else {
-                result = "Hotter!!!";
-            }
-            // store current distance for next round
-            this.distance = currentDistance;
-            // if it is the first guess provide some feedback
-        } else {
-            this.distance = Math.abs(guess - this.secretNumber);
-            if (this.distance >= 1 && this.distance <= 10) {
-                result = "That's damn close!";
-            } else if (this.distance >= 11 && this.distance <= 20) {
-                result = "That's kinda close...";
-            } else {
-                result = "Yeahhhh...Miles away";
-            }
-        }
-        return result;
-    };
-
-    function addPlayerScore(player, totalGuesses) {
-        if (player === player1) {
-            player1.score += totalGuesses;
-            $('.player1 p').text(player1.score);
-        } else {
-            player2.score += totalGuesses;
-            $('.player2 p').text(player2.score);
-        }
-    }
-
-    function startNewRound(player) {
-        if (player.number === 1) {
-            game = new Game(player2);
-            $('.player1 h3').first().slideUp();
-            $('.player2 h3').first().slideDown();
-        } else {
-            game = new Game(player1);
-            $('.player1 h3').first().slideDown();
-            $('.player2 h3').first().slideUp();
-        }
-        //$('#feedback').text(" " + game.currentPlayer.name + " you're up!");
-    }
+    // Game.prototype.validateGuess = function(guess) {
+    //     var result;
+    //     this.totalGuesses++;
+    //     this.currentGuess = guess.toString();
+    //     // check it's an actual number
+    //     if (typeof guess === 'number') {
+    //         // if number is between 1 & 100
+    //         if (guess >= 1 && guess <= 100) {
+    //             //result =
+    //             result = this.giveFeedback(guess);
+    //             // else say so
+    //         } else {
+    //             result = "Your guess needs to be between 1 and 100";
+    //         }
+    //     } else {
+    //         result = "That's not even a number";
+    //     }
+    //     return result;
+    // };
+    // Game.prototype.addGuess = function() {
+    //
+    // };
+    //
+    // Game.prototype.giveFeedback = function(guess) {
+    //     var result;
+    //     // if it's correct...
+    //     if (guess === this.secretNumber) {
+    //         result = "You Got It!!! The number was " + this.secretNumber;
+    //         addPlayerScore(this.currentPlayer, this.totalGuesses);
+    //         startNewRound(this.currentPlayer);
+    //     }
+    //     // check this isn't the first guess
+    //     else if (this.distance > 0) {
+    //         // and see if they've already guessed this number before
+    //         $('#guessList li').each(function() {
+    //             var prevGuess = parseInt($(this).text());
+    //             if (prevGuess === guess) {
+    //                 result = "You've already had that number";
+    //                 return result;
+    //             }
+    //         });
+    //         // if not calculate current guesses distance from the secret number
+    //         var currentDistance = Math.abs(guess - this.secretNumber);
+    //         // if they're equidistant from the answer to their previous guess
+    //         if (currentDistance === this.distance) {
+    //             result = "OOOO! Equidistant!!!";
+    //             // else if further away...
+    //         } else if (currentDistance > this.distance) {
+    //             result = "Colder :(";
+    //             // or closer...
+    //         } else {
+    //             result = "Hotter!!!";
+    //         }
+    //         // store current distance for next round
+    //         this.distance = currentDistance;
+    //         // if it is the first guess provide some feedback
+    //     } else {
+    //         this.distance = Math.abs(guess - this.secretNumber);
+    //         if (this.distance >= 1 && this.distance <= 10) {
+    //             result = "That's damn close!";
+    //         } else if (this.distance >= 11 && this.distance <= 20) {
+    //             result = "That's kinda close...";
+    //         } else {
+    //             result = "Yeahhhh...Miles away";
+    //         }
+    //     }
+    //     return result;
+    // };
+    //
+    // function addPlayerScore(player, totalGuesses) {
+    //     if (player === player1) {
+    //         player1.score += totalGuesses;
+    //         $('.player1 p').text(player1.score);
+    //     } else {
+    //         player2.score += totalGuesses;
+    //         $('.player2 p').text(player2.score);
+    //     }
+    // }
+    //
+    // function startNewRound(player) {
+    //     if (player.number === 1) {
+    //         game = new Game(player2);
+    //         $('.player1 h3').first().slideUp();
+    //         $('.player2 h3').first().slideDown();
+    //     } else {
+    //         game = new Game(player1);
+    //         $('.player1 h3').first().slideDown();
+    //         $('.player2 h3').first().slideUp();
+    //     }
+    //     //$('#feedback').text(" " + game.currentPlayer.name + " you're up!");
+    // }
 
     $(document).ready(function() {
         var model = new Model();
-        var testPlayer = model.makePlayer("Bob");
-        console.log(model.playerList);
+        var player1 = model.makePlayer("Bob");
+        var player2 = model.makePlayer("Linda");
+        model.incrementPlayer();
+        console.log("first log: ", model);
+        model.incrementPlayer();
+        console.log("second log: ",model);
         var userInput = document.getElementById('userGuess');
-        player1 = new Player("Bob", 1);
-        player2 = new Player("Jane", 2);
         startNewRound(player2);
 
         $('.player1 h1').text(player1.name);
